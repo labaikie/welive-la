a303c1c0633ca8f4483854e71da381b651684087
La Baik <labaik237@gmail.com>
2016-02-13 23:09:56 -0800

    building get apartment api with apartment names

--- server/controllers/apartments.js
+++ server/controllers/apartments.js
@@ -4,8 +4,11 @@ var request       = require('request');
 
 module.exports = {
 
+  //
+  // USING SINGLE PROMISE LAYER
+  //
   getApts : function(req, res, next) {
-    var location   = "downtown-los-angeles-los-angeles-ca"; // to be passed in through request
+    var location   = "downtown-los-angeles-los-angeles-ca"; // to be from req.
     var listingUrl = "http://www.apartments.com/apartments/" + location + "/";
     var listing    = {
                         uri: listingUrl,
@@ -13,53 +16,75 @@ module.exports = {
                           return cheerio.load(html);
                         }
                       };
-    var listingPromise = rp(listing).then(function($) {
-      var aptUrls = [];
-      $('article').map(function(i, tag) {
-        var href = $(tag).attr('data-url');
-        if(href) aptUrls.push(href);
+    rp(listing).then(function($) {
+      var apts = [];
+      $('article').map(function(i, element) {
+        var apt = {};
+        if($(element).hasClass('platinum') || $(element).hasClass('gold') || $(element).hasClass('silver')) {
+        var name    = $(element).children().first().text()
+            // url     = $(element).children()[0].attr('href'),
+            // address = $(element).children()[1].children()[1].children()[2].children()[0].text(),
+            // rent    = $(element).children()[1].children()[1].children()[3].children()[0].text(),
+            // unit    = $(element).children()[1].children()[1].children()[3].children()[1].text(),
+            // avail   = $(element).children()[1].children()[1].children()[3].children()[2].text(),
+            // contact = $(element).children()[1].children()[1].children()[4].children()[0].text(),
+            // update  = $(element).children()[1].children()[1].children()[4].children()[1].text()
+          if(name) {
+            apt.name = name;
+            // apt.url = url;
+            // apt.address = address;
+            // apt.rent = rent;
+            // apt.unit = unit;
+            // apt.availability = avail;
+            // apt.contact = contact;
+            // apt.lastupdated = update;
+            apts.push(apt);
+          }
+        } else if($(element).hasClass('bronze')) {
+
+        }
       })
-      return aptUrls
+      res.json(apts)
     })
-    var aptPromises = [];
-    var aptsPromise = listingPromise.then(function(urls){
-      urls.forEach(function(url) {
-        var apt = {
-                    uri: url,
-                    transform: function(html) {
-                      return cheerio.load(html);
-                    }
-                  }
-        aptPromises.push(rp(apt))
-      })
-      return Promise.all(aptPromises)
-    })
-    aptsPromise.then(function(htmls){
-      console.log(htmls)
-    })
-
-
-
-
-
-
-
-
-    // request(listingsUrl, function(err, res, html) {
-    //   var aptUrls = [];
-    //   if(!err) {
-    //     var $ = cheerio.load(html);
-    //     // names = $('article').children().children().first().text();
-    //     $('article').map(function(i, tag) {
-    //       var href = $(tag).attr('data-url');
-    //       if(href) aptUrls.push(href);
-    //     })
-    //   }
-    //   console.log(aptUrls);
-    // })
-
-
-
   }
+
+  //
+  // USING SECOND PROMISE LAYER TO SCRAPE APT DATA
+  //
+  // getApts : function(req, res, next) {
+  //   var location   = "downtown-los-angeles-los-angeles-ca"; // to be from req.
+  //   var listingUrl = "http://www.apartments.com/apartments/" + location + "/";
+  //   var listing    = {
+  //                       uri: listingUrl,
+  //                       transform: function(html) {
+  //                         return cheerio.load(html);
+  //                       }
+  //                     };
+  //   var listingPromise = rp(listing).then(function($) {
+  //     var aptUrls = [];
+  //     $('article').map(function(i, tag) {
+  //       var href = $(tag).attr('data-url');
+  //       if(href) aptUrls.push(href);
+  //     })
+  //     return aptUrls
+  //   })
+  //   var aptPromises = [];
+  //   var aptsPromise = listingPromise.then(function(urls){
+  //     urls.forEach(function(url) {
+  //       aptPromises.push(rp(url));
+  //     })
+  //     return Promise.all(aptPromises);
+  //   })
+  //   aptsPromise.then(function(htmls) {
+  //     htmls.forEach(function(html) {
+  //       var $ = cheerio.load(html);
+  //       var entry = { name:''};
+  //       entry.name = $('.propertyDisplayName').text()
+  //       console.log(entry.name);
+  //     })
+  //   })
+  // }
+
+
 
 }
