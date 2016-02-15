c15c9d17321796342947933490d36554b0e16ced
La Baik <labaik237@gmail.com>
2016-02-14 19:57:41 -0800

    adding mapService to front-end

--- public/www/js/app.js
+++ public/www/js/app.js
@@ -2,12 +2,9 @@
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services', 'app.directives'])
+var app = angular.module('weLiveLA', ['ionic', 'app.controllers', 'app.routes', 'app.services', 'app.directives'])
 
-.run(function($ionicPlatform) {
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
--- public/www/js/services.js
+++ public/www/js/services.js
@@ -1,10 +1,36 @@
 angular.module('app.services', [])
 
-.factory('BlankFactory', [function(){
+.factory('mapService', function($http){
+  return {
 
-}])
+    getAptsPOI : function(location, poi) {
+      var aptPromise = $http({
+        method: GET,
+        url: 'http://localhost:8080/apartments', // || HOSTED SITE
+        data: {location: location}
+      })
+      var poiPromise = $http({
+        method: GET,
+        url: 'http://localhost:8080/poi', // || HOSTED SITE
+        data: {query: poi}
+      })
+      Promise.all([aptPromise, poiPromise]).then(function(data){
+        console.log(data[0]);
+        console.log(data[1]);
+      }).catch(function(err) {
+        console.log(err);
+      })
+    },
 
-.service('BlankService', [function(){
+    getMap : function() {
+      console.log('Google MAP Service')
+    },
 
-}]);
+    getDistance : function() {
+      console.log('Calculate Distance')
+    }
+
+  }
+})
+
 
