4b85b8e36e72568133b7735060aa3e2fbd1c2802
La Baik <labaik237@gmail.com>
2016-02-16 13:26:32 -0800

    adding modal to markers

--- public/www/js/controllers.js
+++ public/www/js/controllers.js
@@ -15,210 +15,202 @@ angular.module('app.controllers', [])
 
 .controller('showSearchCtrl', function($scope, $http, $document, $q, nHService, $ionicModal) {
 
-    $scope.setMap = function(location, poi) {
-      // get map object data from APIs
-      $scope.getMapData(location, poi, function(result){
-                // initialize map
-        var mapOptions = {
-          center: new google.maps.LatLng(34.0483572,-118.2746524),
-          zoom: 14
-        }
-        var map = new google.maps.Map(document.getElementById('map'), mapOptions)
-        var bounds = new google.maps.LatLngBounds();
+  $scope.setMap = function(location, poi) {
+    // get map object data from APIs
+    $scope.getMapData(location, poi, function(result){
+              // initialize map
+      var mapOptions = {
+        center: new google.maps.LatLng(34.0483572,-118.2746524),
+        zoom: 14
+      }
+      var map = new google.maps.Map(document.getElementById('map'), mapOptions)
+      var bounds = new google.maps.LatLngBounds();
 
-        // create and set apartment markers
-        var aptMarkers = [];
-        addAptMarkers();
-        // create and set poi markers
-        var poiMarkers = [];
-        addPOIMarkers();
-        // Map Set to Fit Bounds
-        map.fitBounds(bounds)
+      // create and set apartment markers
+      var aptMarkers = [];
+      addAptMarkers();
+      // create and set poi markers
+      var poiMarkers = [];
+      addPOIMarkers();
+      // Map Set to Fit Bounds
+      map.fitBounds(bounds)
 
-        // Distance Service
+      // Distance Service
 
-        function getDistance(origin){
-          var service = new google.maps.DistanceMatrixService;
-          var poiLL = [];
-          poiMarkers.forEach(function(marker) {
-            var position = marker.getPosition()
-            poiLL.push(position);
-          })
-          var distancePromise = $q(function (resolve, reject) {
-            service.getDistanceMatrix({
-              origins: [origin],
-              destinations: poiLL,
-              travelMode: google.maps.TravelMode.DRIVING,
-              unitSystem: google.maps.UnitSystem.METRIC,
-              avoidHighways: false,
-              avoidTolls: false
-            }, function(res, status) {
-              if(status !== google.maps.DistanceMatrixStatus.OK) {
-                alert('Error was: ' + status);
-              } else {
-                resolve({distance: res.rows[0].elements})
-              }
-            })
-          })
-          // returning promise for eventlistener in addAptMarker to grab
-          return distancePromise;
-        }
-
-        function addPOIMarkers(){
-          // setting POI icon
-          var icon = new google.maps.MarkerImage("./img/black-dot.png")
-          // creating markers
-          var poi = result.poi;
-          poi.forEach(function(poi){
-            var lat = poi.location.coordinate.latitude
-            var lng = poi.location.coordinate.longitude
-            var marker = createMarker(poi.name, icon, lat, lng)
-            poiMarkers.push(marker)
-          })
-          setMarkers(map, poiMarkers);
-        }
-
-        function addAptMarkers(){
-          var apts = result.apartments;
-          // setting apartment icon
-          var icon = new google.maps.MarkerImage("./img/house.png")
-          // pushing Async geocode promises to array
-          var latLngPromises = [];
-          apts.forEach(function(apt){
-            latLngPromises.push(geocode(apt.address))
-          })
-          // after the array of promises is fulfilled, create markers
-          $q.all(latLngPromises).then(function(data){
-            data.forEach(function(d) {
-              var marker = createMarker('apartment', icon, d.lat, d.lng)
-              aptMarkers.push(marker)
-              // addEventListener for DISTANCE & INFO
-              marker.addListener('click', function() {
-                // get distance
-                getDistance(marker.getPosition()).then(function(data){
-                  console.log(data)
-                  // if success, display modal
-
-
-                });
-              })
-            })
-            // set markers on map
-            setMarkers(map, aptMarkers);
-          })
-        }
-
-        function geocode(address) {
-          var geocoder = new google.maps.Geocoder()
-          return $q(function (resolve, reject) {
-            var url_addr = encodeURIComponent(address);
-            geocoder.geocode( {address: address + location}, function(results, status) {
-              if (status == google.maps.GeocoderStatus.OK) {
-                var lat = results[0].geometry.location.lat()
-                var lng = results[0].geometry.location.lng()
-                resolve({ lat: lat, lng: lng})
-              } else {
-                reject("Geocode unsuccessful")
-                console.log("Geocode unsuccessful because: " + status);
-              }
-            });
-          })
-        }
-
-        function createMarker(name, icon, lat, lng) {
-          var latlng = new google.maps.LatLng(lat,lng)
-          var marker = new google.maps.Marker({
-            position: latlng,
-            icon: icon,
-            label: name,
-            title: name
-          })
-          bounds.extend(latlng)
-          return marker;
-        }
-
-        function setMarkers(map, markers) {
-          for(var i = 0; i < markers.length; i++) {
-            markers[i].setMap(map)
-          }
-        }
-
-      })
-
-    };
-
-    $scope.getMapData = function(location, poi, callback) {
-      var result = {apartments:'', poi:''}
-      var aptPromise = $http({
-        method: 'GET',
-        url: 'http://localhost:8080/apartments', // || HOSTED SITE
-        params: {location: location}
-      })
-      var poiPromise = $http({
-        method: 'GET',
-        url: 'http://localhost:8080/poi', // || HOSTED SITE
-        params: {query: poi, location: location}
+      function getDistance(origin){
+        var service = new google.maps.DistanceMatrixService;
+        var poiLL = [];
+        poiMarkers.forEach(function(marker) {
+          var position = marker.getPosition()
+          poiLL.push(position);
         })
-      Promise.all([aptPromise, poiPromise]).then(function(data){
-        result.apartments = data[0].data
-        result.poi = data[1].data
-        callback(result);
-      }).catch(function(err) {
-        console.log(err);
+        var distancePromise = $q(function (resolve, reject) {
+          service.getDistanceMatrix({
+            origins: [origin],
+            destinations: poiLL,
+            travelMode: google.maps.TravelMode.DRIVING,
+            unitSystem: google.maps.UnitSystem.METRIC,
+            avoidHighways: false,
+            avoidTolls: false
+          }, function(res, status) {
+            if(status !== google.maps.DistanceMatrixStatus.OK) {
+              alert('Error was: ' + status);
+            } else {
+              resolve({distance: res.rows[0].elements})
+            }
+          })
+        })
+        // returning promise for eventlistener in addAptMarker to grab
+        return distancePromise;
+      }
+
+      function addPOIMarkers(){
+        // setting POI icon
+        var icon = new google.maps.MarkerImage("./img/black-dot.png")
+        // creating markers
+        var poi = result.poi;
+        poi.forEach(function(poi){
+          var lat = poi.location.coordinate.latitude
+          var lng = poi.location.coordinate.longitude
+          var marker = createMarker(poi.name, icon, lat, lng)
+          poiMarkers.push(marker)
+        })
+        setMarkers(map, poiMarkers);
+      }
+
+      function addAptMarkers(){
+        var apts = result.apartments;
+        // setting apartment icon
+        var icon = new google.maps.MarkerImage("./img/house.png")
+        // pushing Async geocode promises to array
+        var latLngPromises = [];
+        apts.forEach(function(apt){
+          latLngPromises.push(geocode(apt.address))
+        })
+        // after the array of promises is fulfilled, create markers
+        $q.all(latLngPromises).then(function(data){
+          data.forEach(function(d) {
+            var marker = createMarker('apartment', icon, d.lat, d.lng)
+            aptMarkers.push(marker)
+            // addEventListener for DISTANCE & INFO
+            marker.addListener('click', function() {
+              // get distance
+              getDistance(marker.getPosition()).then(function(data){
+                console.log(data)
+                $scope.openModal();
+              });
+            })
+          })
+          // set markers on map
+          setMarkers(map, aptMarkers);
+        })
+      }
+
+      function geocode(address) {
+        var geocoder = new google.maps.Geocoder()
+        return $q(function (resolve, reject) {
+          var url_addr = encodeURIComponent(address);
+          geocoder.geocode( {address: address + location}, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+              var lat = results[0].geometry.location.lat()
+              var lng = results[0].geometry.location.lng()
+              resolve({ lat: lat, lng: lng})
+            } else {
+              reject("Geocode unsuccessful")
+              console.log("Geocode unsuccessful because: " + status);
+            }
+          });
+        })
+      }
+
+      function createMarker(name, icon, lat, lng) {
+        var latlng = new google.maps.LatLng(lat,lng)
+        var marker = new google.maps.Marker({
+          position: latlng,
+          icon: icon,
+          label: name,
+          title: name
+        })
+        bounds.extend(latlng)
+        return marker;
+      }
+
+      function setMarkers(map, markers) {
+        for(var i = 0; i < markers.length; i++) {
+          markers[i].setMap(map)
+        }
+      }
+
+    })
+
+  };
+
+  $scope.getMapData = function(location, poi, callback) {
+    var result = {apartments:'', poi:''}
+    var aptPromise = $http({
+      method: 'GET',
+      url: 'http://localhost:8080/apartments', // || HOSTED SITE
+      params: {location: location}
+    })
+    var poiPromise = $http({
+      method: 'GET',
+      url: 'http://localhost:8080/poi', // || HOSTED SITE
+      params: {query: poi, location: location}
       })
-    };
+    Promise.all([aptPromise, poiPromise]).then(function(data){
+      result.apartments = data[0].data
+      result.poi = data[1].data
+      callback(result);
+    }).catch(function(err) {
+      console.log(err);
+    })
+  };
 
-    $scope.geocode = function(address) {
-      var geocoder = new google.maps.Geocoder()
-      return $q(function (resolve, reject) {
-        var url_addr = encodeURIComponent(address);
-        geocoder.geocode( {address: address + location}, function(results, status) {
-          if (status == google.maps.GeocoderStatus.OK) {
-            var lat = results[0].geometry.location.lat()
-            var lng = results[0].geometry.location.lng()
-            resolve({ lat: lat, lng: lng})
-          } else {
-            reject("Geocode unsuccessful")
-            console.log("Geocode unsuccessful because: " + status);
-          }
-        });
-      })
-    }
-
-    // TESTER LINE
-    $scope.setMap(nHService.current, 'Grocery');
-
-
-
-
-
-
-
-    // defining modal
-    $ionicModal.fromTemplateUrl('previewModal.html', {
-      scope: $scope,
-      animation: 'slide-in-up'
-      }).then(function(modal) {
-        $scope.modal = modal;
+  $scope.geocode = function(address) {
+    var geocoder = new google.maps.Geocoder()
+    return $q(function (resolve, reject) {
+      var url_addr = encodeURIComponent(address);
+      geocoder.geocode( {address: address + location}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          var lat = results[0].geometry.location.lat()
+          var lng = results[0].geometry.location.lng()
+          resolve({ lat: lat, lng: lng})
+        } else {
+          reject("Geocode unsuccessful")
+          console.log("Geocode unsuccessful because: " + status);
+        }
       });
-    $scope.openModal = function() {
-      $scope.modal.show();
-    };
-    $scope.closeModal = function() {
-      $scope.modal.hide();
-    };
-    //Cleanup the modal when we're done with it!
-    $scope.$on('$destroy', function() {
-      $scope.modal.remove();
+    })
+  }
+
+  // TESTER LINE
+  $scope.setMap(nHService.current, 'Grocery');
+
+  // Define Modal
+  $ionicModal.fromTemplateUrl('templates/preview-modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope = modal;
     });
-    // Execute action on hide modal
-    $scope.$on('modal.hidden', function() {
-      // Execute action
-    });
-    // Execute action on remove modal
-    $scope.$on('modal.removed', function() {
-      // Execute action
-    });
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
 
 })
 
