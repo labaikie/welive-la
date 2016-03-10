a6aea38f737f5464cfcb3c1270fbb4132aa3e9b0
La Baik <labaik237@gmail.com>
2016-03-09 23:09:00 -0800

    adding dotenv and environment variables for yelp

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@
 //
 var express       = require('express');
 var app           = express();
+var dotenv        = require('dotenv');
+dotenv.config();
 var bodyParser    = require('body-parser');
 var morgan        = require('morgan');
 var mongoose      = require('mongoose');
